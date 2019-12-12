49aa39ef0916cdf88f110759ee1a24da67faf431
kadirumac <kadirumac49@gmail.com>
2019-12-12 10:56:58 +0100

    exported

--- modules/click.js
+++ modules/click.js
@@ -1,9 +1,11 @@
-function clickImages() {
+export function clickImages() {
     const sectionContent = document.getElementById("content");
     sectionContent.addEventListener("click", e => {
+        console.log("target", e.target)
         switch (e.target.tagName) {
             case "IMG":
             alert("All of the yarn is available in our shop!")
         }
     })
  }
+
